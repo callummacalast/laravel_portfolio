20fd7815a73cc2b6fc2766c287b5d35bde774bc4
Callum Macalast <callummacalast@callums-mbp-2.home>
2022-05-03 21:32:12 +0100

    Changes to the about section and learn cta

--- app/View/Components/Home/Portfolio.php
+++ app/View/Components/Home/Portfolio.php
@@ -2,10 +2,16 @@
 
 namespace App\View\Components\Home;
 
+use Illuminate\Support\Arr;
 use Illuminate\View\Component;
 
 class Portfolio extends Component
 {
+
+    public array $items = [];
+
+    public array $tabs = [];
+
     /**
      * Create a new component instance.
      *
@@ -13,7 +19,27 @@ class Portfolio extends Component
      */
     public function __construct()
     {
-        //
+        $this->items = [
+            [
+                'category' => ['Laravel', 'Tailwind.css', 'Vue3'],
+                'title' => 'Full Stack app with Laravel, Tailwindcss and Vue 3',
+                'image' => url('/img/survey.png'),
+                'github' => 'https://github.com/thecodeholic/laravel-vue-survey'
+            ],
+            [
+                'category' => ['WordPress', 'Bootsrap 5', 'JS'],
+                'title' => 'Full Stack app with Laravel, Tailwindcss and Vue 3',
+                'image' => url('/img/survey.png'),
+                'github' => 'https://github.com/thecodeholic/laravel-vue-survey'
+            ],
+            [
+                'category' => ['WordPress', 'Bootsrap 5', 'JS'],
+                'title' => 'Full Stack app with Laravel, Tailwindcss and Vue 3',
+                'image' => url('/img/survey.png'),
+                'github' => 'https://github.com/thecodeholic/laravel-vue-survey'
+            ],
+        ];
+        $this->tabs = array_unique(Arr::flatten(Arr::pluck($this->items, 'category')));
     }
 
     /**
--- resources/views/components/home/about.blade.php
+++ resources/views/components/home/about.blade.php
@@ -7,14 +7,14 @@
                     <div class="w-full xl:w-1/2 px-3 sm:px-4">
                         <div class="py-3 sm:py-4">
                             <img
-                                src="{{ url('/img/me1.png') }}"
+                                src="{{ url('/img/me3.jpeg') }}"
                                 alt=""
                                 class="rounded-2xl w-full"
                             />
                         </div>
                         <div class="py-3 sm:py-4">
                             <img
-                                src="{{ url('/img/me3.png') }}"
+                                src="{{ url('/img/me1.jpeg') }}"
                                 alt=""
                                 class="rounded-2xl w-full"
                             />
@@ -23,7 +23,7 @@
                     <div class="w-full xl:w-1/2 px-3 sm:px-4">
                         <div class="my-4 relative z-10">
                             <img
-                                src="{{ url('/img/me2.png') }}"
+                                src="{{ url('/img/me2.jpeg') }}"
                                 alt=""
                                 class="rounded-2xl w-full"
                             />
